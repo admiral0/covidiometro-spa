f4bb5fdf5e3407624fbb5e8f42c164d28b6e72c0
Radu Andries <me@admiral0.it>
2021-01-23 22:50:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is the frontend of covidiometro.it
---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import About from "./routes/About.svelte";
+	import Dashboard from "./routes/Dashboard.svelte";
+
+	import Router from 'svelte-spa-router'
+
+	const routes = {
+		'/': Dashboard,
+		'/about': About,
+
+		'*': Dashboard,
+	}
+
+</script>
+
+<nav>
+	<a href="#/">Home</a>
+	<a href="#/about">About</a>
+</nav>
+
+<Router {routes} />
+
+<style>
+
+</style>
